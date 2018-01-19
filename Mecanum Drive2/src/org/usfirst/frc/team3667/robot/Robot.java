af6175386e678df4dd39fab94c8057fa082a8e76
Anonymous <32804557+FyreAlchemage@users.noreply.github.com>
2018-01-18 20:15:06 -0500

    Re-add files
    There, it's fixed

--- Mecanum Drive2/src/org/usfirst/frc/team3667/robot/Robot.java
+++ Mecanum Drive2/src/org/usfirst/frc/team3667/robot/Robot.java
@@ -1,19 +1,21 @@
 package org.usfirst.frc.team3667.robot;
 
+// encoder import
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
-
+// other imports
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import com.analog.adis16448.frc.ADIS16448_IMU;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import com.ctre.CANTalon.TalonControlMode;
-import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
@@ -25,23 +27,24 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class Robot extends IterativeRobot {
 	// Command autonomousCommand;
-	public static final double kDistancePerRevolution = 18.84; // guestimate
-																// from your
-																// code
-	public static final double kPulsesPerRevolution = 1024; // for an AS5145B
-															// Magnetic Encoder
+
+	// gyro variables
+	// private Gyro gyro;
+
+	public static final double kDistancePerRevolution = 18.84;
+	public static final double kPulsesPerRevolution = 1024;
+
 	public static final double kDistancePerPulse = kDistancePerRevolution / kPulsesPerRevolution;
 	private Encoder leftEncoder = new Encoder(0, 1, true, EncodingType.k4X);
 	private Encoder rightEncoder = new Encoder(2, 3, false, EncodingType.k4X);
-	private RobotDrive drive = new RobotDrive(1, 2);
 
 	Timer autoTime = new Timer();
-	CANTalon _frontLeftMotor = new CANTalon(13);
-	CANTalon _frontRightMotor = new CANTalon(12);
-	CANTalon _rearRightMotor = new CANTalon(11);
-	CANTalon _rearLeftMotor = new CANTalon(10);
-	CANTalon _climber = new CANTalon(14);
-	RobotDrive _drive = new RobotDrive(_frontLeftMotor, _rearLeftMotor, _frontRightMotor, _rearRightMotor);
+	WPI_TalonSRX _frontLeftMotor = new WPI_TalonSRX(13);
+	WPI_TalonSRX _frontRightMotor = new WPI_TalonSRX(12);
+	WPI_TalonSRX _rearRightMotor = new WPI_TalonSRX(11);
+	WPI_TalonSRX _rearLeftMotor = new WPI_TalonSRX(10);
+	WPI_TalonSRX _climber = new WPI_TalonSRX(14);
+	MecanumDrive _drive = new MecanumDrive(_frontLeftMotor, _rearLeftMotor, _frontRightMotor, _rearRightMotor);
 	Joystick _joy = new Joystick(0);
 	Joystick _climberJoy = new Joystick(1);
 
@@ -51,16 +54,14 @@ public class Robot extends IterativeRobot {
 	 */
 	public void robotInit() {
 
-		_drive.setInvertedMotor(MotorType.kFrontRight, true);
-		_drive.setInvertedMotor(MotorType.kRearRight, true);
 		// autonomousCommand = new move() ;
-		
-		//start of encoders
+
+		// start of encoders
 		leftEncoder.setDistancePerPulse(kDistancePerPulse);
-        rightEncoder.setDistancePerPulse(kDistancePerPulse);
-        leftEncoder.reset();
-        rightEncoder.reset();
-        
+		rightEncoder.setDistancePerPulse(kDistancePerPulse);
+		leftEncoder.reset();
+		rightEncoder.reset();
+
 	}
 
 	/**
@@ -68,36 +69,55 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 
-		_drive.mecanumDrive_Cartesian(_joy.getRawAxis(0), _joy.getRawAxis(1), _joy.getRawAxis(4), 0);
+		_drive.driveCartesian(_joy.getRawAxis(0), _joy.getRawAxis(1), _joy.getRawAxis(4), 0);
 		if (_joy.getRawAxis(2) != 0) {
 			_climber.set(_joy.getRawAxis(2));
 		} else {
 			_climber.set(_joy.getRawAxis(3) * -1.0);
 		}
-		
-		
-		//encoder logic
-		if (_joy.getRawButton(2) == true)
-		{
+
+		// encoder logic
+		if (_joy.getRawButton(2) == true) {
 			leftEncoder.reset();
 			rightEncoder.reset();
 		}
 		SmartDashboard.putNumber("encoderL", leftEncoder.getDistance());
 		SmartDashboard.putNumber("encoderR", rightEncoder.getDistance());
-		
+
 		// Display button values
 		SmartDashboard.putBoolean("button1", _joy.getRawButton(1));
 		SmartDashboard.putBoolean("button2", _joy.getRawButton(2));
+
+		// Gyro logic
+		/*
+		 * if (_joy.getRawButton(1) == true) { gyro.reset(); } double angle =
+		 * gyro.getAngle(); SmartDashboard.putNumber("angle", angle);
+		 */
 	}
 
-	public void autonomousInit()
-	{
+	public void autonomousInit() {
 		SmartDashboard.putString("statusInit", "Initialize auton");
 	}
-	
-	public void autonomousPeriodic()
-	{
-		SmartDashboard.putString("status", "Auton periodic");
+
+	public void autonomousPeriodic() {
+		int sdNum = 0;
+		//Sendable sdNum = SmartDashboard.getData("Decision");
+		switch (sdNum) {
+		case 0: {
+			if (Timer.getMatchTime() > 9.75) {
+				_drive.driveCartesian(0, 0, -0.5, 0);
+			} else {
+				_drive.driveCartesian(0, -.1, 0, 0);
+			}
+			SmartDashboard.putNumber("Timer", Timer.getMatchTime());
+			break;
+		}
+		case 1: 
+			{
+				
+				break;
+			}
+		}
 	}
 
 	// public void autonomous() {
