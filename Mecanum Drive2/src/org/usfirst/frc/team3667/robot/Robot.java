af2a8376392586a4ae83aee46d6a8e75e5e6c9f9
ochudson <33849917+ochudson@users.noreply.github.com>
2018-01-31 19:35:44 -0500

    added drive function that sets minimum speed

--- Mecanum Drive2/src/org/usfirst/frc/team3667/robot/Robot.java
+++ Mecanum Drive2/src/org/usfirst/frc/team3667/robot/Robot.java
@@ -49,6 +49,10 @@ public class Robot extends IterativeRobot {
 	Joystick _joy = new Joystick(0);
 	Joystick _climberJoy = new Joystick(1);
 
+	int autonStep = 1;
+
+	double lastValidDirection = 0;
+
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -78,7 +82,7 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during operator control
 	 */
 	public void teleopPeriodic() {
-		_drive.mecanumDrive_Cartesian(_joy.getRawAxis(0), _joy.getRawAxis(1), _joy.getRawAxis(4), 0);
+		adjustedDrive(_joy.getRawAxis(0), _joy.getRawAxis(1), _joy.getRawAxis(4), 0);
 		if (_joy.getRawAxis(2) != 0) {
 			_climber.set(_joy.getRawAxis(2));
 		} else {
@@ -101,6 +105,11 @@ public class Robot extends IterativeRobot {
 	private double getDegreeZ() {
 		double someDegree;
 		someDegree = imu.getAngleZ();
+		return normalizeDegree(someDegree);
+	}
+
+	// very nice method that we definitely *did* create
+	private double normalizeDegree(double someDegree) {
 		while (someDegree < 0 || someDegree >= 360) {
 			if (someDegree >= 360) {
 				someDegree -= 360;
@@ -121,6 +130,8 @@ public class Robot extends IterativeRobot {
 		imu.reset();
 		leftEncoder.reset();
 		rightEncoder.reset();
+		autonStep = 1;
+		lastValidDirection = 0;
 	}
 
 	public void autonomousPeriodic() {
@@ -128,54 +139,201 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("IMU Angle Z", getDegreeZ());
 		SmartDashboard.putNumber("encoderL", leftEncoder.getDistance());
 		SmartDashboard.putNumber("encoderR", rightEncoder.getDistance());
-		int planNum = 2;
+		SmartDashboard.putNumber("Last Valid Direction", lastValidDirection);
+		SmartDashboard.putNumber("Auton Step", autonStep);
+		SmartDashboard.putNumber("Actual Gryo", imu.getAngleZ());
+		int planNum = 5;
 		switch (planNum) {
 		case 0: {
 			if (Timer.getMatchTime() > 10.5) {
-				_drive.mecanumDrive_Cartesian(0, -.3, 0, 0);
+				adjustedDrive(0, -.3, 0, 0);
 			} else if (Timer.getMatchTime() > 9.75) {
-				_drive.mecanumDrive_Cartesian(0, 0, 0.5, 0);
+				adjustedDrive(0, 0, 0.5, 0);
 			} else {
-				_drive.mecanumDrive_Cartesian(0, -.1, 0, 0);
+				adjustedDrive(0, -.1, 0, 0);
 			}
 			break;
 		}
 		case 1: {
 			if (Timer.getMatchTime() > 10.5) {
-				_drive.mecanumDrive_Cartesian(0, -.3, 0, 0);
+				adjustedDrive(0, -.3, 0, 0);
 			} else if (Timer.getMatchTime() > 9.75) {
-				_drive.mecanumDrive_Cartesian(0, 0, -0.5, 0);
+				adjustedDrive(0, 0, -0.5, 0);
 			} else {
-				_drive.mecanumDrive_Cartesian(0, -.1, 0, 0);
+				adjustedDrive(0, -.1, 0, 0);
 			}
 			break;
 		}
 		case 2: {
 
 			if (leftEncoder.getDistance() < 120 && rightEncoder.getDistance() < 120) {
-				driveForward();
+				adjustedDrive(0, -.4, 0, 0);
 			} else if (leftEncoder.getDistance() <= 122 && rightEncoder.getDistance() >= 120) {
-				_drive.mecanumDrive_Cartesian(0, 0, -0.5, 0);
+				adjustedDrive(0, 0, -0.5, 0);
 			} else if (leftEncoder.getDistance() <= 107 && rightEncoder.getDistance() >= 136) {
-				_drive.mecanumDrive_Cartesian(0, -.1, 0, 0);
+				adjustedDrive(0, -.1, 0, 0);
 			} else if (leftEncoder.getDistance() >= 112 && rightEncoder.getDistance() >= 141) {
 				_climber.set(.5);
 			}
 			break;
 		}
 		case 3: {
-
+			// from right starting position-right switch
+			switch (autonStep) {
+			case 1: {
+				driveForward(-.6, 49);
+				autonStep++;
+				break;
+			}
+			case 2:
+				// turnRobot(270);
+				turnLeft(90, 50);
+				autonStep++;
+				break;
+			}
+		}
+		case 4: {
+			// from right starting position-right switch
+			switch (autonStep) {
+			case 1: {
+				driveForward(-.6, 49);
+				autonStep++;
+				break;
+			}
+			case 2:
+				// turnRobot(270);
+				turnRight(90, 50);
+				autonStep++;
+				break;
+			}
+		}
+		case 5: {
+			// from right starting position-right scale
+			switch (autonStep){
+			case 1: {
+				driveForward(-.8, 60);
+				autonStep++;
+				break;
+			}
+			}
+		}
 		}
 
-			SmartDashboard.putNumber("Timer", Timer.getMatchTime());
+		SmartDashboard.putNumber("Timer", Timer.getMatchTime());
+	}
+
+	private void adjustedDrive(double xAxis, double yAxis, double rotation, double gyroAngle) {
+		double curHeight = 0;
+		// Round to the nearest Hundredth to remove game pad inaccuracy 
+		double adjXAxis = Math.round(xAxis * 10.0) / 10.0;
+		double adjYAxis = Math.round(yAxis * 10.0) / 10.0;
+		double adjRotation = Math.round(rotation * 10.0) / 10.0;
+		double adjGyroAngle = Math.round(gyroAngle * 10.0) / 10.0;
+		double minSpeedNum = .1;
+		double maxSpeedNum = 1;
+		if (curHeight < 10) {
+			adjXAxis = Math.round(xAxis * 10.0) / 10.0;
+			adjYAxis = Math.round(yAxis * 10.0) / 10.0;
+			adjRotation = Math.round(rotation * 10.0) / 10.0;
+			adjGyroAngle = Math.round(gyroAngle * 10.0) / 10.0;
+		} else if (curHeight < 20) {
+			adjXAxis = xAxis * .75;
+			adjYAxis = yAxis * .75;
+			adjRotation = rotation * .75;
+			adjGyroAngle = gyroAngle * .75;
+		} else {
+			adjXAxis = xAxis * .5;
+			adjYAxis = yAxis * .5;
+			adjRotation = rotation * .5;
+			adjGyroAngle = gyroAngle * .5;
 		}
+		if (Math.abs(adjXAxis) < minSpeedNum && adjXAxis != 0) {
+			if (adjXAxis > 0) {
+				adjXAxis = minSpeedNum;
+
+			} else {
+				adjXAxis = minSpeedNum * -1;
+			}
+		}
+		if (Math.abs(adjYAxis) < minSpeedNum && adjYAxis != 0) {
+			if (adjYAxis > 0) {
+				adjYAxis = minSpeedNum;
+
+			} else {
+				adjYAxis = minSpeedNum * -1;
+			}
+		}
+
+		if (Math.abs(adjRotation) < minSpeedNum && adjRotation != 0) {
+			if (adjRotation > 0) {
+				adjRotation = minSpeedNum;
+
+			} else {
+				adjRotation = minSpeedNum * -1;
+			}
+		}
+		if (Math.abs(adjGyroAngle) < minSpeedNum && adjGyroAngle != 0) {
+			if (adjGyroAngle > 0) {
+				adjGyroAngle = minSpeedNum;
+
+			} else {
+				adjGyroAngle = minSpeedNum * -1;
+			}
+		}
+
+		_drive.mecanumDrive_Cartesian(adjXAxis, adjYAxis, adjRotation, adjGyroAngle);
+	}
+
+	private void turnRight(double turnDegrees, double speedPercent) {
+		double curDegree = imu.getAngleZ();
+		double targetDegree = lastValidDirection + turnDegrees;
+
+		while (targetDegree > imu.getAngleZ()) {
+			SmartDashboard.putNumber("target Degree", targetDegree);
+
+			if (Math.abs(targetDegree - imu.getAngleZ()) > 10) { // check if
+																	// within 10
+																	// degrees
+																	// and if so
+																	// slow turn
+				SmartDashboard.putNumber("speed", speedPercent * .01);
+				_drive.mecanumDrive_Cartesian(0, 0, speedPercent * .01, 0);
+			} else {
+				_drive.mecanumDrive_Cartesian(0, 0, .25, 0);
+			}
+		}
+		SmartDashboard.putNumber("Actual Gryo", imu.getAngleZ());
+		lastValidDirection -= turnDegrees;
+	}
+
+	private void turnLeft(double turnDegrees, double speedPercent) {
+		double curDegree = imu.getAngleZ();
+		double targetDegree = lastValidDirection - turnDegrees;
+
+		while (targetDegree < imu.getAngleZ()) {
+			SmartDashboard.putNumber("target Degree", targetDegree);
+
+			if (Math.abs(targetDegree - imu.getAngleZ()) > 10) { // check if
+																	// within 10
+																	// degrees
+																	// and if so
+																	// slow turn
+				SmartDashboard.putNumber("speed", speedPercent * -.01);
+				_drive.mecanumDrive_Cartesian(0, 0, speedPercent * -.01, 0);
+			} else {
+				_drive.mecanumDrive_Cartesian(0, 0, -.25, 0);
+			}
+		}
+		SmartDashboard.putNumber("Actual Gryo", imu.getAngleZ());
+		lastValidDirection -= turnDegrees;
 	}
 
 	private void driveForward(double travelSpeed, double travelDistance) {
 		double startingLeftEncoder = leftEncoder.getDistance();
 		double startingRightEncoder = rightEncoder.getDistance();
-		while (leftEncoder.getDistance() <= startingLeftEncoder + travelDistance && rightEncoder.getDistance() <= startingRightEncoder + travelDistance){
-		_drive.mecanumDrive_Cartesian(0, -.4, 0, 0);
+		while (leftEncoder.getDistance() <= startingLeftEncoder + travelDistance
+				&& rightEncoder.getDistance() <= startingRightEncoder + travelDistance) {
+			_drive.mecanumDrive_Cartesian(0, travelSpeed, 0, 0);
 		}
-}
+	}
 }
